778a42cf4bc187f34802aaafeab919c89c7a0ac3
Amoeba-Hau <hau.nguyen@amoeba.group>
2024-07-31 17:30:38 +0700

    second commit

--- src/pages/PhotoCard.vue
+++ src/pages/PhotoCard.vue
@@ -1,18 +1,50 @@
 <script setup>
-    import { ref } from 'vue'
+    import { ref, onMounted } from 'vue'
+    import axios from 'axios'
 
-    defineProps({
-        msg: String,
+    const cards = ref([]);
+
+    const fetchCards = async () => {
+      try {
+        const response = await axios.get('https://66aa0468613eced4eba734ec.mockapi.io/api/v1/cards');
+        cards.value = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    onMounted(async () => {
+        await fetchCards()
     })
-
-    const count = ref(0)
 </script>
 
 <template>
-    <section id="cards">
-        Photo cards
+    <section class="cards">
+        <h2 class="cards__breadcrumb">Photo Cards</h2>
+        <div class="cards__container">
+            <div v-for="(card, index) in cards" :key="index" class="card__item">
+                <div class="card__img">
+                    <img :src="card.image" :alt="card.title">
+                </div>
+                <h3 class="card__title">
+                    {{ card.title }}
+                </h3>
+            </div>
+        </div>
     </section>
 </template>
-<style scoped lang="scss">
 
+<style scoped lang="scss">
+    .cards {
+        &__container {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            gap: 0 15px;
+        }
+        &__item {
+            
+        }
+    }
 </style>
